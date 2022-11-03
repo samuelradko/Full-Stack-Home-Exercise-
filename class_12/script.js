3e8554dc311861fd46a5533bcaa792e25c81a25d
samuelradko <samuelradko@gmail.com>
2022-11-03 20:21:29 +0200

    file upload

---
+++ class_12/script.js
@@ -0,0 +1,42 @@
+// First question 
+
+let Pie = (Math.PI > 10)
+console.log(Pie);
+
+
+// Second Question 
+
+let y = (Math.PI)
+console.log(Math.round(y));
+
+// third Question 
+
+let z = 15 != 10;
+console.log(z);
+
+// fourth Question
+
+let equile = 50 == 50;
+console.log(equile);
+
+// Fifth Question 
+
+let odd = -18 < 13;
+console.log(odd);
+
+// Question Six 
+
+function result(x) {
+    if (x == 30) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+console.log(result())
+
+
+
+
